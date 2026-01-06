a2fccf599e8b3214aed762d387b62b1e84d87e5b
disikoX <damien.zara12@gmail.com>
2026-01-06 16:56:44 +0300

    [FIX]: typo error

--- Python/main.py
+++ Python/main.py
@@ -4,7 +4,7 @@ import math
 import re
 from dataclasses import dataclass, asdict
 from typing import Dict, List, Tuple, Optional
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 # -----------------------------
